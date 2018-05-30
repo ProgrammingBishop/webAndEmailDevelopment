57fd2e92e0b2392287e10724e372b381fb7d4337
Scott Bishop <sbishop@officite.com>
2018-05-30 09:22:16 -0500

    Rearranging file tree

--- reactPractice/weather/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import WeatherReducer from './reducer_weather';
-
-const rootReducer = combineReducers({
-	// Set the weather reducer to contain output of reducer_weather.js returned state
- 	weather : WeatherReducer
-});
-
-export default rootReducer;
